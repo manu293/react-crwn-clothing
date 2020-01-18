19e4e5bd0d8864822108a0d1ed2a60dc484cabbd
Manoj S M <manojsm293@gmail.com>
2020-01-18 16:42:04 +0530

    Added redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware, compose } from "redux";
+/* eslint-disable no-undef */
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 
-import logger from "redux-logger";
-import rootReducer from "./root-reducer";
+import logger from 'redux-logger';
+import rootReducer from './root-reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [logger];
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
